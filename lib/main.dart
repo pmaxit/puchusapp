3e8a716996c4355f28e0ca925f1a977807f3a450
Puneet Girdhar <puneetg@meta.com>
2023-07-25 11:35:03 -0500

    added searchbar

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
         '/second': (context) => const TestPageSecond(),
         '/third': (context) => TestPageThird(),
       },
-      initialRoute: '/',
+      initialRoute: '/third',
     );
   }
 }
